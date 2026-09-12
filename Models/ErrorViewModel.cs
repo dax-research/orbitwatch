1e43b5e2c097b4cb3c5bba87b7d06eb23d2183d5
dax-research <dax1214b@gmail.com>
2026-09-12 18:31:12 +0530

    Initial ASP.NET Core MVC project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OrbitWatch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
