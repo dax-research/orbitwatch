16ecddf37f09e0d1d73aeb2d92cbee9bfaa084f9
dax-research <dax1214b@gmail.com>
2026-09-15 17:21:40 +0530

    Add GroundStation Model

---
+++ Models/GroundStation.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrbitWatch.Models
+{
+    public class GroundStation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Location { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Country { get; set; }
+
+        [StringLength(100)]
+        public string Operator { get; set; }
+
+        [StringLength(50)]
+        public string Status { get; set; }
+    }
+}
