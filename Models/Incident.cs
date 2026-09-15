2e3529162a062de4216549dce4955458e115821c
dax-research <dax1214b@gmail.com>
2026-09-15 17:17:06 +0530

    Add Incident Model

---
+++ Models/Incident.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrbitWatch.Models
+{
+    public class Incident
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int SatelliteId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Type { get; set; }
+
+        [Required]
+        [StringLength(500)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime OccurredAt { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Severity { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Status { get; set; }
+
+        // Navigation property
+        public Satellite Satellite { get; set; }
+    }
+}
